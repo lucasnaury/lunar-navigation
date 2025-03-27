67f3ae26ab80e48b71cb48309591a04229b99f39
Nikhilanand Jha <nikhiljha_29@yahoo.com>
2025-03-27 14:42:13 +0000

    Camera plugin added, might not work on jazzy, local path planning.py rolled back to the initial version

--- euro2moon/launch/local_path_planning.py
+++ euro2moon/launch/local_path_planning.py
@@ -32,41 +32,7 @@ def generate_launch_description():
 
         # Local path planning nodes
         # ...
-        def generate_launch_description():
-    package_dir = get_package_share_directory('my_package')
-
-
-# Get robot
-    robot_description_path = os.path.join(package_dir, 'resource', 'my_robot.urdf')
-
-# Get map
-    webots = WebotsLauncher(
-        world=os.path.join(package_dir, 'worlds', 'my_world.wbt')
-    )
-
-    my_robot_driver = WebotsController(
-        robot_name='my_robot',
-        parameters=[
-            {'robot_description': robot_description_path},
-        ]
-    )
-
-    obstacle_avoider = Node(
-        package='my_package',
-        executable='obstacle_avoider',
-    )
-
-    return LaunchDescription([
-        webots,
-        my_robot_driver,
-        obstacle_avoider,
-        launch.actions.RegisterEventHandler(
-            event_handler=launch.event_handlers.OnProcessExit(
-                target_action=webots,
-                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
-            )
-        )
-    ])
+        
 
     ])
      
