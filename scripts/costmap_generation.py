5be8543a744bc515ee66731381ce3ebe829bc85c
Lucas NAURY <lucas.naury@sfr.fr>
2025-03-28 11:49:49 +0000

    Slope map from heightmap

--- scripts/costmap_generation.py
+++ scripts/costmap_generation.py
@@ -3,66 +3,60 @@ import os
 from pathlib import Path
 import numpy as np
 import cv2
-from stl.mesh import Mesh
-import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
-
-
-# ---------------------------------------------------------------- #
-#                            VISUALISATION
-# ---------------------------------------------------------------- #
-def plotMesh(mesh:Mesh):
-    # Show STL model
-    figure = plt.figure()
-    axes = figure.add_subplot(projection='3d')
-
-    # Load the STL files and add the vectors to the plot
-    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(mesh.vectors))
-
-    # Auto scale to the mesh size
-    scale = mesh.points.flatten()
-    axes.auto_scale_xyz(scale, scale, scale)
-
-    # Show the plot to the screen
-    plt.show()
-
+from include.helpers import showImage
 
 
 
 # ---------------------------------------------------------------- #
 #                          INDIVIDUAL MAPS
 # ---------------------------------------------------------------- #
-def getSlopeMap(mesh, size):
-
-    # Initialise empty map
-    slopeMap = np.zeros((size, size))
+def getSlopeMap(heightmap, method):
     
-    # Extract normal vectors
-    normals = mesh.normals  # Shape (N, 3), where N is the number of triangles
-
-    # Define the horizontal plane normal (z-up)
-    horizontal = np.array([0, 0, 1])
-
-    # Compute slopes as the angle between the triangle normal and the horizontal
-    cos_theta = np.einsum('ij,j->i', normals, horizontal) / np.linalg.norm(normals, axis=1)
-    cos_theta = np.clip(cos_theta, -1, 1)  # Avoid numerical issues
-
-    slopes = np.abs(np.arccos(cos_theta))  # Absolute radians
-
-    # Remove mesh offset
-    # faces = mesh.vectors[:,:,0:2]
-    # faces -= np.min(np.min(faces, axis=1), axis=0)
+    diag = 1/np.sqrt(2)
+    sum = 1*4 + diag*4
+    kernel = np.array([[-diag, -1, -diag],
+                       [-1,    sum,   -1],
+                       [-diag, -1, -diag]]) / sum
     
-    # Calculate normal cell index
-    normalsPositions = np.floor(np.average(faces, axis=1)).astype(int) # Get cell index based on face center
-    
-    # Store average of slope (2 triangles for 1 cell) in cell
-    np.add.at(slopeMap, (normalsPositions[:, 0], normalsPositions[:, 1]), slopes/2)
+    # kernel = np.array([[0, -1, 0],
+    #                 [-1, 1, -1],
+    #                 [0, -1, 0]])
 
+
+    slopeMap = None
+    if method == 0: # My test
+        slopeMap = cv2.filter2D(heightmap, -1, kernel)
+        slopeMap = np.absolute(slopeMap)
+
+    elif method == 1: # NUMPY GRADIENT
+        slopeMap = np.gradient(heightmap,edge_order=1)
+        slopeMap = np.absolute(slopeMap)
+        slopeMap = cv2.addWeighted(slopeMap[0], 0.5, slopeMap[1], 0.5, 0)
+
+    elif method == 2: # TEST cv2 SOBEL
+        gX = cv2.Sobel(heightmap, cv2.CV_64F, dx=1, dy=0, ksize=5)
+        gY = cv2.Sobel(heightmap, cv2.CV_64F, dx=0, dy=1, ksize=5)
+
+        gX = cv2.convertScaleAbs(gX)
+        gY = cv2.convertScaleAbs(gY)
+        # combine the gradient representations into a single image
+        slopeMap = cv2.addWeighted(gX, 0.5, gY, 0.5, 0)
+
+
+    # slopeMap = cv2.GaussianBlur(slopeMap,(21,21),5)
+    slopeMap = cv2.blur(slopeMap,(15,15))
+
+
+    print(slopeMap)
+    print(slopeMap.shape)
+
+    
+
+    
     return slopeMap
 
 
-def getIlluminationMap(mesh):
+def getIlluminationMap():
     
     # Get images folder
     path = str(Path().parent.absolute() / "illumination")
@@ -92,38 +86,39 @@ def getIlluminationMap(mesh):
 # ---------------------------------------------------------------- #
 #                                MAIN
 # ---------------------------------------------------------------- #
-def main(path):
-    # Import moon surface STL file
-    print("Loading STL file...")
-    mesh = Mesh.from_file(path)
-    print(Path().parent.absolute())
+def main(path, elevationLow, elevationHigh):
+    # Import moon surface heightmap
+    heightmap = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    print("Heightmap dim:", heightmap.shape)
 
-    # Plot 3D mesh
-    # plotMesh(mesh)
+    # Set 0 height to for unknown data
+    # heightmap = np.where(heightmap > 253, 0, heightmap)
 
+    showImage("Heightmap", heightmap)
 
-
+    heightmap = heightmap * (elevationHigh - elevationLow) / 255.0 + elevationLow
+    # heightmap = cv2.blur(heightmap,(3,3))
+    # heightmap = heightmap.astype(np.float64) / 255.0
+    
     # Initialise costmap
-    outputDim = int(np.floor(np.sqrt(mesh.vectors.shape[0] / 2)))
-    print("Output dimension:", outputDim)
-
-    costMap = np.zeros((outputDim, outputDim))
+    costMap = np.zeros(heightmap.shape)
     
 
     # -> Slope map
-    slopeMap = getSlopeMap(mesh, outputDim)
+    slopeMapsNormalised = np.zeros((3,heightmap.shape[0],heightmap.shape[1]))
+    for i in range(3):
+        slopeMap = getSlopeMap(heightmap,i)
 
-    slopeNormalised = (slopeMap - np.min(slopeMap)) / (np.max(slopeMap) - np.min(slopeMap))
+        slopeMapsNormalised[i] = (slopeMap - np.min(slopeMap)) / (np.max(slopeMap) - np.min(slopeMap))
 
-    print("Slope map: ",slopeMap)
-    print("Normalised map: ",slopeNormalised)
-    showImage("Slope map", slopeNormalised)
+        # print("Slope map: ",slopeMap)
+        # showImage("Slope map", slopeMap)
+        # print("Normalised map: ",slopeNormalised)
+        showImage(f"Slope map {i} normalised", slopeMapsNormalised[i])
 
-
-
-    # -> Illumination map
-    illuminationMap = getIlluminationMap(mesh)
-    showImage("Illumination map", illuminationMap)
+    # # -> Illumination map
+    # illuminationMap = getIlluminationMap()
+    # showImage("Illumination map", illuminationMap)
 
 
 
@@ -138,8 +133,9 @@ def main(path):
 
 if __name__ == "__main__":
     # Check if STL path is given as arg
-    filepath = sys.argv[1] if len(sys.argv) > 1 else "../euro2moon\worlds\models\moon_surface\moon_cropped.stl"
-    # filepath = sys.argv[1] if len(sys.argv) > 1 else "C://Users//lucas//Desktop//surface_test.stl"
+    filepath = sys.argv[1] if len(sys.argv) > 1 else "heightmap_screen.png"
+    elevationLow = sys.argv[2] if len(sys.argv) > 2 else -8225
+    elevationHigh = sys.argv[3] if len(sys.argv) > 3 else 3700
     
     # Run program
-    main(filepath)
+    main(filepath, elevationLow, elevationHigh)
---
+++ scripts/include/helpers.py
@@ -0,0 +1,16 @@
+def showImage(windowName, img, max_dim=650):
+    """Show a scaled down version of an image"""
+    import cv2
+
+    cv2.namedWindow(windowName,cv2.WINDOW_KEEPRATIO)
+    cv2.imshow(windowName, img)
+    
+
+    if img.shape[1] > img.shape[0]:
+        newWidth = max_dim
+        newHeight = int(newWidth / img.shape[1] * img.shape[0])
+    else:        
+        newHeight = max_dim
+        newWidth = int(newHeight / img.shape[0] * img.shape[1])
+
+    cv2.resizeWindow(windowName, (newWidth, newHeight))
