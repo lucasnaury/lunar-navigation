6d93d6ec56c1c666d83c7df113dedf0e1352b560
Nikhilanand Jha <nikhiljha_29@yahoo.com>
2025-04-20 11:08:59 +0100

    Rename fake path publisher

--- euro2moon/setup.py
+++ euro2moon/setup.py
@@ -65,7 +65,7 @@ setup(
             # 'obstacle_avoider = my_package.obstacle_avoider:main'
             'my_robot_driver = euro2moon.my_robot_driver:main',
             'obstacle_avoider = euro2moon.obstacle_avoider:main',
-            'fake_path = euro2moon.fake_path_publisher3:main',
+            'fake_path = euro2moon.fake_path_publisher:main',
             'plotter = euro2moon.trajectory_plotting:main'
         ],
     },
